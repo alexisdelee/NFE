e14c400921ae46e8526464bf9a24d46c4502ab6c
Alexis Delee <alexisdelee@protonmail.com>
2019-03-04 11:25:05 +0100

    add method for asynchronous methods in express.js

--- api/decorators/wrapper.ts
+++ api/decorators/wrapper.ts
@@ -9,3 +9,17 @@ export function makecoffee(target, property: string, descriptor: PropertyDescrip
 
     return descriptor;
 }
+
+export function unicornStuff(target, property: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+    const method = descriptor.value;
+
+    descriptor.value = function() {
+        return Q.async(function(request, response, next) {
+            (Q as any).spawn(function *() {
+                yield Q.async(method).call(this, request, response, next).catch(next);
+            });
+        }).apply(this, arguments);
+    };
+
+    return descriptor;
+}
