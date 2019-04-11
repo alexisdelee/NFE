98f1e6c4a7007d173e62d300acc610dafe3d5d76
Francois <email>
2019-04-11 17:59:24 +0200

    Created incidents creation form and back office connection form

--- web/index.js
+++ web/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+const Sentry = require('@sentry/node');
+Sentry.init({ dsn: 'https://d547fae43622419b9dc6a012c385b410@sentry.io/1432720' });
+app.use(Sentry.Handlers.requestHandler());
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next) {
+    res.statusCode = 500;
+    res.end(res.sentry + '\n');
+});
 
 app.listen(80, 'nfe.fr', () => {
     console.log('Started on port 80');
 });
 
+app.use(bodyParser.json());
+
 app.use('/', express.static('./public'));
+
 app.use('/declaration_incidents', express.static('./public/html/declaration_incidents.html'));
 app.use('/connexion_back_office', express.static('./public/html/connexion_back_office.html'));
+app.use('/backoffice', express.static('./public/html/backoffice'));
+
+app.post('/login', (req, res) => {
+    if (req.body.email_nfe == 'francois@mail.fr' && req.body.nfeid == "FBAS" && req.body.password == "axios") {
+        res.json({
+            success: true
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
+});
--- web/public/js/index.js
+++ web/public/js/index.js
@@ -1,1 +1,54 @@
-$('#header').load('../header.html');
+/*var s = new Vue({
+    el: '#degradation_type',
+    data: {
+      options: [
+          { text: 'Panne de réseau', value: 'panne' },
+          { text: 'Compteur cassé', value: 'compteur_cassee'}
+      ]
+    }
+});*/
+
+var email_nfe = new Vue({
+    el: '#email_nfe'
+});
+
+var nfeid = new Vue({
+    el: '#nfeid'
+});
+
+var password = new Vue({
+    el: '#password'
+});
+
+var connection_button = new Vue({
+    el: '#connection_button',
+    methods: {
+        connect: function() {
+        var email = getVal(email_nfe).trim();
+        var id = getVal(nfeid).trim();
+        var pass = getVal(password).trim();
+
+        if (email == "" || id == "" || pass == "") {
+            alert("imcomplet");
+            return;
+        }
+
+        axios.post(
+            "/login",
+            {
+                email_nfe: email,
+                nfeid: id,
+                password: pass
+            }
+        ).then(function(res) {
+            alert(res.data.success);
+
+
+        })
+    }
+  }
+});
+
+function getVal(element) {
+    return element.$el.value;
+}
